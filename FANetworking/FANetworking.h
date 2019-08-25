53a560fc732a264a96692b9f0cdc51a0c77c1a05
Farzan <far.abd8050@gmail.com>
2019-08-25 17:53:18 +0430

    Initial Commit

---
+++ FANetworking/FANetworking.h
@@ -0,0 +1,19 @@
+//
+//  FANetworking.h
+//  FANetworking
+//
+//  Created by Farzan on 8/25/19.
+//  Copyright © 2019 Farzan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FANetworking.
+FOUNDATION_EXPORT double FANetworkingVersionNumber;
+
+//! Project version string for FANetworking.
+FOUNDATION_EXPORT const unsigned char FANetworkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FANetworking/PublicHeader.h>
+
+
